a8c42dfa07f20687ea89972d4b7a246083db3292
xiaodounai <695004450@qq.com>
2023-04-16 20:48:49 +0800

    变更

--- 410homework_ARP/day1_gratuitous_arp.py
+++ 410homework_ARP/day1_gratuitous_arp.py
@@ -2,19 +2,20 @@ import logging
 
 logging.getLogger("kamene.runtime").setLevel(logging.ERROR)  # 清除报错
 from kamene.all import *
-from /tools/get_mac_netifaces import get_mac_address
 
+from get_mac_netifaces import get_mac_address
+from scapy_iface import scapy_iface
 
-def gratuituous_arp(ipaddress, ifname):
+def gratuituous_arp(ip_address, ifname):
     localmac = get_mac_address(ifname)
-    gratuituous_arp.pkt = Ether(src=localmac,
+    gratuitous_arp_pkt = Ether(src=localmac,
                                 dst='ff:ff:ff:ff:ff:ff')/ARP(op=2,
                                                              hwsrc=localmac,
                                                              hwdst=localmac,
                                                              psrc=ip_address,
                                                              pdst=ip_address)
-    sendp(gratuitous_arp_pkt ,iface=scapy_iface(ifname), verbos=False)
+    sendp(gratuitous_arp_pkt ,iface=scapy_iface(ifname), verbose=False)
 
 if __name__ == "__main__":
-    gratuitous_arp('10.1.1.1', ifname='ens160')
+    gratuituous_arp('10.1.1.1', ifname='ens160')
 
